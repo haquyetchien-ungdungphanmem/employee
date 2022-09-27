802630fd4d1065c73b7556b394a299eec091fb02
haquyetchien-ungdungphanmem <haquyetchien.ungdungphanmem@gmail.com>
2022-09-27 11:43:57 +0700

    commit cors

--- QLNV-Project/src/main/java/com/example/qlnvproject/authConfig/AuthConfig.java
+++ QLNV-Project/src/main/java/com/example/qlnvproject/authConfig/AuthConfig.java
@@ -48,7 +48,7 @@ public class AuthConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
-        http.cors().and().csrf().disable();
+        http.cors().and().csrf();
 
         http.authorizeHttpRequests().antMatchers("/auth/**").permitAll()
                 .antMatchers("/role/**").hasRole("giamdoc")
